c859253bba8af51056eeccbed048fa305de987d3
Radoslaw <struradek196@gmail.com>
2023-01-07 05:05:14 +0100

    add_profile_list

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('profile_list/', views.profile_list, name='profile-list'),
 ]
